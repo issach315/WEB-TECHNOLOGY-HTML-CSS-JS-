017b7817dd03759d093307a2615fb39de88cf858
issach315 <issachemani227@gmial.com>
2023-02-16 11:03:45 +0530

    data types in js

---
+++ JavaScript/DataTypes/datatypes.js
@@ -0,0 +1,35 @@
+/* Js is a dynamcallly typed lang and 
+ it will assign the data type by dynamivally  during the initialisation 
+ 
+ the datatypes are 
+  > number 
+  > String
+  > boolean
+  > undefined
+  > null
+ */
+
+  var x=10
+  var x1=10.1
+
+  var y='Issach'
+  var z=true
+
+  console.log(typeof(x))
+  console.log(typeof(x1))
+
+  console.log(typeof(y))
+  console.log(typeof(z))
+
+
+/*  Composite DataTypes in Js
+1.array
+2.object
+
+*/
+
+var name = ['issach','Nani','praveen']
+
+
+
+  
