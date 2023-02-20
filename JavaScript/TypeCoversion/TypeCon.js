d13d1c6481a22d30ac9514fb318648b31040ab8b
issach315 <issachemani227@gmial.com>
2023-02-20 15:30:35 +0530

    type conversion Number String & Boolean

--- JavaScript/TypeCoversion/TypeCon.js
+++ JavaScript/TypeCoversion/TypeCon.js
@@ -15,19 +15,23 @@ var x='993JDBC'
 var y=parseInt(x)
 console.log(y)
 console.log(typeof(y));
-console.log('______________________')
+console.log('__________Number____________')
 
-var j=Number(true)
+ var j=Number(false)// Number(true)  Number(flase)  Number("hi")--> NaN [not a number]
 console.log(j)
 console.log(typeof(j))
 
-var z=32
-z=String(z)
+
+
+
+console.log("_______String________")
+
+ let z=String(true) // String(false) String(1234)  String(null) String(undefined) String('kill')
 console.log(z)
 console.log(typeof(z))
 
 
-
-var c=Boolean(null)
+console.log('_________Boolean_________')
+var c=Boolean("hi")  //Boolean(1234) Boolean(false) Boolean(true) Boolean(null) Boolean('') 
 console.log(c)
 console.log(typeof(c))
