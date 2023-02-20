1b23bf93ae661512b17d0e3026241fa409d51c57
issach315 <issachemani227@gmial.com>
2023-02-20 15:13:15 +0530

    Datatype conversion

--- JavaScript/TypeCoversion/TypeCon.js
+++ JavaScript/TypeCoversion/TypeCon.js
@@ -6,3 +6,28 @@ console.log(12%'3')
 console.log(12-'s')
 console.log(false-12)
 console.log(12+true)
+
+// Explicit conversion
+
+// Number String boolean
+
+var x='993JDBC'
+var y=parseInt(x)
+console.log(y)
+console.log(typeof(y));
+console.log('______________________')
+
+var j=Number(true)
+console.log(j)
+console.log(typeof(j))
+
+var z=32
+z=String(z)
+console.log(z)
+console.log(typeof(z))
+
+
+
+var c=Boolean(null)
+console.log(c)
+console.log(typeof(c))
