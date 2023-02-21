753ac4606fb3a3e1a39ff7dd32e2d307afda0468
issach315 <issachemani227@gmial.com>
2023-02-21 19:07:46 +0530

    switch case

---
+++ JavaScript/Conditional/SwitchCase.js
@@ -0,0 +1,21 @@
+var month=2;
+switch (month)
+ {
+    case 1:
+        console.log("jan")
+        break;
+     case 2:
+        console.log("feb")
+        break;
+     case 3:
+         console.log("mar")
+         break;
+     case 4:
+         console.log("aprl")
+         break;
+                
+
+    default:
+        console.log('hello india')
+        break;
+}
