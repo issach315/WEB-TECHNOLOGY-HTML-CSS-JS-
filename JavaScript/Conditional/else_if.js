29dc96f182ec363380c35a08da4527c291e41d6e
issach315 <issachemani227@gmial.com>
2023-02-21 19:07:02 +0530

    else if in js

---
+++ JavaScript/Conditional/else_if.js
@@ -0,0 +1,22 @@
+var i=3;
+
+if (i==1)
+ {
+    console.log('hello')
+    
+}
+else if (i==2)
+ {
+    console.log('hello1')
+    
+}
+else if (i==3)
+{
+   console.log('hello2')
+   
+}
+else if (i==4)
+{
+   console.log('hello3')
+   
+}
