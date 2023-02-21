f7462585f016d618eb18d930d9dad1fabf5e1e4d
issach315 <issachemani227@gmial.com>
2023-02-21 19:09:29 +0530

    simple for loo

---
+++ JavaScript/Loops/forLoop..js
@@ -0,0 +1,14 @@
+
+        for (let index = 0; index <10; index++)
+         {
+            for (let index1 = 0; index1 <index; index1++)
+         {
+           
+                console.lo("* ")
+            
+        }
+      
+       
+        }
+
+        
