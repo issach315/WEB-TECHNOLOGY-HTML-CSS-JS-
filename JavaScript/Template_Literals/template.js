4cd8d466ee28db1b87f4737a5ee291f8ce4314e8
issach315 <issachemani227@gmial.com>
2023-02-21 19:10:51 +0530

    template literals

---
+++ JavaScript/Template_Literals/template.js
@@ -0,0 +1,14 @@
+console.log(`hello
+     issach`)// for next line 
+
+var x=17;
+var y=88;
+console.log(`sum of ${x} and ${y} is : ${x+y}`);
+
+console.log(`hey "issach" hello how are you`);
+
+
+console.log("hellow \"issach\"");
+
+
+     
